9edf1fb9a475f8175c947b6ff67c529c53968fcb
Hytálo Benaventana <pd_troia@hotmail.com>
2025-03-29 22:31:30 -0300

    feat: state machine without delay in progress

--- blink.ino
+++ blink.ino
@@ -1,7 +1,8 @@
 int i = 0;
 int pin = digitalRead(4) == 1 ? 2 : 5;
 int prevPin = pin; 
-
+int rollTime = 0;
+int actualRollTime = 0;
 
 
 void setup() {
@@ -13,10 +14,11 @@ void setup() {
   pinMode(4, INPUT_PULLUP);  // Habilita resistor pull-up interno
 }
 void blink(int pin){
-  delay(250);
-  digitalWrite(pin, HIGH);
-  delay(250);
-  digitalWrite(pin, LOW);  
+  int blinkTime = 250;
+  actualRollTime + blinkTime >= rollTime ?  digitalWrite(pin, HIGH) :  digitalWrite(pin, LOW);  
+  if (actualRollTime + blinkTime * 2 >= rollTime){
+    actualRollTime = rollTime; 
+  }
 }
 void loop() {  
   blink(pin);
@@ -27,5 +29,5 @@ void loop() {
   if(digitalRead(4) == LOW){
     pin = pin == 2 ? 5 : 2;
   }
-  delay(50);
+  rollTime = millis();
 }
