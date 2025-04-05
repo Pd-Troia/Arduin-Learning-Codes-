dad39da9f2bd40aed2d7215ec991122a709ce1b3
Hytálo Benaventana <pd_troia@hotmail.com>
2025-04-05 12:13:49 -0300

    fix: state machine working correctly, but only if println on pin exists, cause ???

--- blink.ino
+++ blink.ino
@@ -1,10 +1,9 @@
 int i = 0;
-int pin = digitalRead(4) == 1 ? 2 : 5;
+int pin = digitalRead(4);
 int prevPin = pin; 
-int rollTime = 0;
-int actualRollTime = 0;
-
-
+unsigned long int rollTime = 0;
+unsigned long int actualRollTime = 0;
+int flag = 0;
 void setup() {
   Serial.begin(9600);
   pinMode(2, OUTPUT);
@@ -13,21 +12,39 @@ void setup() {
   digitalWrite(5, LOW);
   pinMode(4, INPUT_PULLUP);  // Habilita resistor pull-up interno
 }
+
 void blink(int pin){
   int blinkTime = 250;
-  actualRollTime + blinkTime >= rollTime ?  digitalWrite(pin, HIGH) :  digitalWrite(pin, LOW);  
-  if (actualRollTime + blinkTime * 2 >= rollTime){
-    actualRollTime = rollTime; 
+  int pinHigh = pin ? 2 : 5;
+  int pinLow = !pin ? 2 : 5;
+  if(rollTime - actualRollTime > blinkTime){
+    digitalWrite(pinHigh, HIGH); 
+    digitalWrite(pinLow, LOW); 
+  }else{
+    digitalWrite(pinHigh, LOW);
+    digitalWrite(pinLow, LOW);
   }
+  if ( rollTime - actualRollTime > blinkTime * 2){
+    actualRollTime = rollTime; 
+  }  
+}
+int onOutButton(int* flag){  
+  int buttonPinState = digitalRead(4);  
+  if(buttonPinState == 1 && *flag == 1){
+    *flag = 0;       
+    return 1;
+  }
+  if(*flag == 0 && buttonPinState == 0){
+    *flag = 1;   
+  }
+  return 0;
 }
 void loop() {  
   blink(pin);
-  if (pin != prevPin){
-    digitalWrite(prevPin, LOW);
-    prevPin = pin; 
-  }
-  if(digitalRead(4) == LOW){
-    pin = pin == 2 ? 5 : 2;
-  }
-  rollTime = millis();
+  Serial.println(pin);
+  int isButtonOutPressed = onOutButton(&flag); 
+  if(isButtonOutPressed){
+     pin = !pin;
+  } 
+  rollTime = millis(); 
 }
